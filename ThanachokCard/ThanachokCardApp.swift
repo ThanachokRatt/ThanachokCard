19c6a83e95af4bf706af52c021bdf732fb756188
Thanachok Rattanamora <schweppeweb@gmail.com>
2023-03-14 17:02:58 +0700

    Initial Commit

---
+++ ThanachokCard/ThanachokCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThanachokCardApp.swift
+//  ThanachokCard
+//
+//  Created by Schweppe on 14/3/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct ThanachokCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

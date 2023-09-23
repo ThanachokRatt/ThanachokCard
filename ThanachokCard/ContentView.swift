2b948856eaf0f50dc49920f0b892319ca8cfe092
thanachok <ext.thanachok.rat@truedigital.com>
2023-09-23 19:20:35 +0700

    hero add a

--- ThanachokCard/ContentView.swift
+++ ThanachokCard/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
                         Circle()
                             .stroke(Color.white,lineWidth : 5))
 
-                Text("Thanachok Rattanamora")
+                Text("Thanachok Rattanamoraa")
                     .font(Font.custom("Pacifico-Regular", size: 32))
                     .bold()
                 .foregroundColor(.white )
